20e8de9ab4e7973d0e121812d1fe7a578e128370
Reaper <kmr.arnav3484@gmail.com>
2024-03-28 10:01:44 +0530

    Added functionality to draw a chess board on the screen

--- main.py
+++ main.py
@@ -4,10 +4,33 @@ from pygame.sprite import Sprite, Group
 
 pygame.init()
 
-width = 500
-height = 500
+square_size = 120
+width = square_size * 8
+height = square_size * 8
 
 screen = pygame.display.set_mode((width, height))
+
+light_square_color = '#7D955C'
+dark_square_color = '#EEEFD4'
+
+# Draw Squares
+square_list = []
+for i in range(8):
+    for j in range(8):
+        square_surf = pygame.Surface((square_size, square_size))
+        square_rect = square_surf.get_rect(topleft = (i * square_size, j * square_size))
+        if ((i % 2 == 0 and j % 2 == 0) or (i % 2 == 1 and j % 2 == 1)):
+            square_surf.fill(light_square_color)
+        else:
+            square_surf.fill(dark_square_color)
+
+        square_list.append((square_surf, square_rect))
+
+
+def draw_board():
+    for square in square_list:
+        screen.blit(square[0], square[1])
+
 
 clock = pygame.time.Clock()
 running = True
@@ -17,12 +40,13 @@ while running:
         if event.type == QUIT:
             running = False
 
-    screen.fill('white')
+    screen.fill('gray')
+
+    draw_board()
 
     clock.tick(60)
     pygame.display.update()
 
 pygame.quit()
-    
 
 
